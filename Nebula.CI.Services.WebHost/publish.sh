00877e2cd38848b2f406a72ea5c6865da31e0b98
xiaolait <xiaolait@outlook.com>
2020-10-17 18:14:49 +0800

    fix bugs

--- Nebula.CI.Services.WebHost/publish.sh
+++ Nebula.CI.Services.WebHost/publish.sh
@@ -1,4 +1,9 @@
 #! /bin/bash
 
+cd ./ClientApp && \
+  cp ./static/antd.css ./node_modules/ant-design-vue/dist && \
+  cp ./static/codemirror.css ./node_modules/codemirror/lib && \
+  npm run build && cp -r ./dist/* ../wwwroot/ && cd ..
+
 dotnet publish -o ./bin/Release
 docker build -t nebula/ci/services .
