4148749b2b6e5cc0e84cc4097d18bcc98f0c2549
xiaolait <xiaolait@outlook.com>
2020-11-06 13:26:34 +0800

    update reference

--- Nebula.CI.Services.WebHost/publish.sh
+++ Nebula.CI.Services.WebHost/publish.sh
@@ -6,6 +6,7 @@ rm -r ./bin
 cd ./ClientApp && npm install && \
   cp ./static/antd.css ./node_modules/ant-design-vue/dist && \
   cp ./static/codemirror.css ./node_modules/codemirror/lib && \
+  cp ./static/Pipeline.vue ./node_modules/vue-pipeline/src/components && \
   npm run build && cp -r ./dist/* ../wwwroot/ && cd ..
 
 dotnet publish -o ./bin/Release
